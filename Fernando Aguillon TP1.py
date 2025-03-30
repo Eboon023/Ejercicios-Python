7e460235e32dc6bdc30d85ed7f068e3e3e46f4a3
Fernando <fernando.j.aguillon.b@gmail.com>
2025-03-30 11:15:45 -0300

    Primer commit

---
+++ Fernando Aguillon TP1.py
@@ -0,0 +1,50 @@
+#Ejercicio 1
+#print("Hola Mundo!")
+
+#Ejercicio 2
+#nombre = input("Por favor ingrese su nombre: ")
+#print("Hola ", nombre, "!", sep="")
+
+#Ejercicio 3
+#nombre = input("Por favor ingrese su nombre: ")
+#apellido = input("Su apellido: ")
+#edad = input("Su edad: ")
+#lugar = input("Y por ultimo su lugar de residencia: ")
+#print("Soy ", nombre, " ", apellido, ", tengo ", edad, " y vivo en ", lugar, sep="")
+
+#Ejercico 4
+#radio = int(input("Por favor ingresar el radio del circulo a calcular: "))
+#print(radio)
+#print(f"El area del circulo es: {3.14 * radio ** 2:.2f}, y su perimetro es: {3.14 * radio * 2:.2f}", sep="")
+
+#Ejercico 5
+#segundos = int(input("Ingrese una cantidad de segundios: "))
+#print(f"Esto equivale a {segundos // 3600} horas, {(segundos % 3600) // 60} minutos, {(segundos % 3600) % 60} segundos")
+
+#Ejercico 6
+#num = int(input("Por favor ingrese el numero de la tabla de multiplicar que quiere que se muestre: "))
+#print(f"Tabla de multiplicar del {num}:\n1 x {num} = {num}\n2 x {num} = {num*2}\n3 x {num} = {num*3}\n4 x {num} = {num*4}\n5 x {num} = {num*5}\n6 x {num} = {num*6}\n7 x {num} = {num*7}\n8 x {num} = {num*8}\n9 x {num} = {num*9}\n10 x {num} = {num*10}")
+
+#Ejercico 7
+#num1 = int(input("Ingrese un numero diferente a cero: "))
+#num2 = int(input("Ingrese otro numero diferente a cero: "))
+#print(f"{num1} + {num2} = {num1+num2}\n{num1} - {num2} = {num1-num2}\n{num1} * {num2} = {num1*num2}\n{num1} / {num2} = {num1/num2:.2f}\n")
+
+#Ejercico 8
+#altura = float(input("Ingrese su altura en metros: "))
+#peso = float(input("Ingrese su peso en Kg: "))
+#print(f"Su IMC es: {peso / (altura ** 2):.2f}")
+
+#Ejercicio 9
+#temp = float(input("Ingrese la temperatura en Celsius: "))
+#print(f"La temperatura equivale a {temp * (9 / 5) + 32} F°")
+
+#Ejercico 10
+#a= int(input("Ingrese un numero: "))
+#b= int(input("Ingrese otro numero: "))
+#c= int(input("Ingrese un ultimo numero: "))
+#print(f"El promedio es: {(a + b + c) / 3:.2f}")
+
+
+#Otra linea mas
+#Una mas y ya
